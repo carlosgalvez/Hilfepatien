d14e463e26ee6f55e9f49e3c09d9620081af374d
RicardoMendoza <ricky_m120@hotmail.com>
2016-10-14 09:22:41 -0500

    se crearon las clases Empleados y Receta

---
+++ Hilfepatien/HilfepatienApi/Models/Empleados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HilfepatienApi.Models
+{
+    public class Empleados
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Puesto { get; set; }
+        public DateTime Fecha_Ingreso { get; set; }
+        public int Sueldo { get; set; }
+
+    }
+    }

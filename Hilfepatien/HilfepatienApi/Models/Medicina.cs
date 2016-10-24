fcd73877b324fef960462a426c185915ede3eb69
carlosgalvez <carlosgalvez9707@hotmail.com>
2016-10-23 19:16:51 -0500

    Se agregaron las ultimas clases

---
+++ Hilfepatien/HilfepatienApi/Models/Medicina.cs
@@ -0,0 +1,19 @@
+﻿using HilfepatienApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HilfepatienMvc.Models
+{
+    public class Medicina
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int IdProveedor { get; set; }
+        public string Presentacion { get; set; }
+        public string TipodeMedicamento { get; set; }
+        public virtual ICollection<Proveedor>Proveedor{ get; set; }
+
+    }
+}
